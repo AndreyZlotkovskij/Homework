05b4b7cb76b7f872b487c9439a60c371e0c50e80
Andrey Zlotkovskiy <zlotkovskija@gmail.com>
2024-05-17 15:20:03 +0300

    .

--- 1-HOMEWORKpoJS/script.js
+++ 1-HOMEWORKpoJS/script.js
@@ -1,10 +1,7 @@
-// Считываем имя пользователя
 const userName = prompt("Please enter your name:");
 
-// Считываем возраст пользователя
 const userAge = parseInt(prompt("Please enter your age:"));
 
-// Проверяем возраст и взаимодействуем с пользователем в соответствии с условиями
 if (userAge < 18) {
   alert("You are not allowed to visit this website.");
 } else if (userAge >= 18 && userAge <= 22) {
--- 3-HOMEWORKpoJS/script.js
+++ 3-HOMEWORKpoJS/script.js
@@ -1,4 +1,3 @@
-// Функция для выполнения математической операции
 function calculate(num1, num2, operator) {
     switch (operator) {
       case '+':
@@ -8,7 +7,6 @@ function calculate(num1, num2, operator) {
       case '*':
         return num1 * num2;
       case '/':
-        // Проверка деления на 0
         if (num2 === 0) {
           return 'Деление на 0 невозможно';
         }
@@ -22,11 +20,9 @@ function calculate(num1, num2, operator) {
   const userInput1 = parseFloat(prompt("Введите первое число:"));
   const userInput2 = parseFloat(prompt("Введите второе число:"));
   
-  // Проверить, что пользователь ввел числа
   if (isNaN(userInput1) || isNaN(userInput2)) {
     console.log("Одно или оба введенных значения не являются числами. Пожалуйста, попробуйте еще раз.");
   } else {
-    // Запросить у пользователя математическую операцию
     const userOperator = prompt("Введите математическую операцию (+, -, *, /):");
   
     // Выполнить операцию и вывести результат в консоль
